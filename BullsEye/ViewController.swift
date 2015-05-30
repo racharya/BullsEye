f19bf722342d93b9fc8b3f3fea98c89c5df89ec1
Rachana Acharya <rachanaacharya@u.boisestate.edu>
2015-05-30 17:20:02 -0600

    Setup project; add UI Controls

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Rachana Acharya on 5/16/15.
+//  Copyright (c) 2015 Rachana Acharya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+
+@IBAction func showAlert(){
+    let alert = UIAlertController(title: "Hello, World", message: "This is my first app!", preferredStyle: .Alert)
+    let action = UIAlertAction(title: "Awesome", style: .Default, handler: nil)
+    
+    alert.addAction(action)
+    presentViewController(alert, animated: true, completion: nil)
+    
+    }
+    
+    }
